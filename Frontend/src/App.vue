2bd4fbf9eda21b97674c7d4a215106632134352a
Michael Hovgaard <hovgaard1990@gmail.com>
2025-03-24 15:28:13 +0100

    frontend install + tailwind 3.3.3.
    cleaned up, removed helloworld, installed tailwind 3.3.3.

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="text-red-500 text-2xl font-bold">
+    Hello OnlinePos
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
